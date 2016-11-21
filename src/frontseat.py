67e2b3e12052f1a74f17e9f37c37a9dc8d293316
Axel <axel-sundbom@hotmail.com>
2016-11-21 16:21:39 +0100

    Adds more frontend

--- src/frontseat.py
+++ src/frontseat.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python
 from flask import Flask, render_template
+from flask_navigation import Navigation
 
 app = Flask(__name__)
+nav = Navigation(app) # setup flask navigation
 
-@app.route("/")
+nav.Bar('top', [
+	nav.Item('Home', 'index'),
+	nav.Item('About', 'about'),
+	nav.Item('My Profile', 'profile'),
+	nav.Item('Lounge', 'lounge')
+])
+
+@app.route('/')
 def index ():
-	return render_template("index.html")
+	return render_template('index.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/profile')
+@app.route('/profile/<string:name>')
+def profile(name=''):
+	return render_template('profile.html', pname=name)
+
+@app.route('/lounge')
+@app.route('/lounge/<string:name>')
+def lounge(name=''):
+	return render_template('lounge.html', lname=name)
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=8080, debug=True)
