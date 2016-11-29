7e28114b945151431b8aa20d005cb7634fd76764
relaxz <relaxz@live.se>
2016-11-29 14:11:44 +0100

    Added stuff, like sidebar and templates

---
+++ src/config.py
@@ -0,0 +1,5 @@
+class Config:
+    api_url = None
+
+    def __init__(self):
+        self.api_url = "http://127.0.0.1:5000"
--- src/frontseat.py
+++ src/frontseat.py
@@ -1,34 +1,113 @@
 #!/usr/bin/python
-from flask import Flask, render_template
+import requests
+import json
+from config import Config
+from flask import Flask, render_template, request
 from flask_navigation import Navigation
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
+config = Config()
 app = Flask(__name__)
-nav = Navigation(app) # setup flask navigation
+nav = Navigation(app)  # setup flask navigation
 
 nav.Bar('top', [
-	nav.Item('Home', 'index'),
-	nav.Item('About', 'about'),
-	nav.Item('My Profile', 'profile'),
-	nav.Item('Lounge', 'lounge')
+    nav.Item('Home', 'index'),
+    nav.Item('Search', 'search'),
+    nav.Item('Browse', 'browse'),
+    nav.Item('Playlist', 'playlist'),
+    nav.Item('Radio', 'radio'),
+    nav.Item('Info', 'info', items=[
+        nav.Item('About', 'about'),
+        nav.Item('Contact', 'contact'),
+    ]),
+    nav.Item('Profile', 'profile', items=[
+        nav.Item('Settings', 'settings'),
+        nav.Item('Signout', 'signout'),
+    ]),
 ])
 
+
 @app.route('/')
-def index ():
-	return render_template('index.html')
+def index():
+    return render_template('index.html', title='Home', icon='home')
+
+
+@app.route('/search')
+def search():
+    return render_template('search.html', title='Search')
+
+
+@app.route('/browse')
+@app.route('/browse/<string:name>')
+def browse(name=''):
+    return render_template('browse.html', title='Browse', lname=name)
+
+
+@app.route('/playlist')
+def playlist():
+    return render_template('playlist.html', title='Playlist')
+
+
+@app.route('/radio')
+def radio():
+    return render_template('radio.html', title='Radio')
+
+
+@app.route('/info')
+def info():
+    return render_template('info.html', title='info')
+
 
 @app.route('/about')
 def about():
-	return render_template('about.html')
+    return render_template('about.html', title='About us')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contact us')
+
 
 @app.route('/profile')
 @app.route('/profile/<string:name>')
 def profile(name=''):
-	return render_template('profile.html', pname=name)
+    return render_template('profile.html', pname=name)
 
-@app.route('/lounge')
-@app.route('/lounge/<string:name>')
-def lounge(name=''):
-	return render_template('lounge.html', lname=name)
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html', title='Settings')
+
+
+@app.route('/signout')
+def signout():
+    return render_template('signout.html', title='Sign out')
+
+
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        data = {"username": form.username.data, "email": form.email.data, "password": form.password.data}
+        url = config.api_url + "/api/register/" + data['username']
+        headers = {'Content-Type': 'application/json'}
+        r = requests.post(url, data = json.dumps(data), headers = headers)
+
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
