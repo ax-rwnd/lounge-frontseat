83585ad5df2527881038cf6d9c14cb33feba5f74
relax <relaxz@live.se>
2016-12-07 13:30:40 +0100

    Added stuff like login and playlist.

---
+++ src/seated.py
@@ -0,0 +1,35 @@
+import requests
+import json
+from flask import url_for
+
+
+def send_post(config, endpoint, data):
+    '''sends a request to rest api
+    configuration contain api url
+    endpoint describes a resource'''
+    url = config.api_url + endpoint
+    headers = {'Content-Type': 'application/json'}
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+
+    return r.json()
+
+
+def send_get(config, endpoint):
+    '''send a request to rest api
+    configuration contain api url
+    endpoint describes a resource'''
+    url = config.api_url + endpoint
+    r = requests.get(url)
+    return r.json()
+
+
+def send_file(config, endpoint, data):
+    fp = requests.files["name_of_file"]
+    url = create_upload_url(url_for('handle_upload_response'))
+    response = requests.post(url, {'file':
+                                       (fp.filename, fp.stream,
+                                        fp.content_type, fp.headers)})
+    if response == "Success":
+        return "File uploaded successfully"
+    else:
+        return "Something didn't work out"
